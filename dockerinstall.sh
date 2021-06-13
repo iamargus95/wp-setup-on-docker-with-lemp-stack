29b873b9c0e33b88fdc3871467fedec3cf9055c7
iamargus95 <kamathsuraj95@gmail.com>
2021-06-13 14:10:13 +0530

    reworking the yaml file

--- dockerinstall.sh
+++ dockerinstall.sh
@@ -5,12 +5,12 @@ echo "Checking for a Docker and Docker-Compose installation."
 
 # Verify if docker is installed.
 if ! docker --version; then 
-       echo "ERROR: Docker not found."
-       echo "Installing Docker from get.docker.com"
-       curl -fsSL https://get.docker.com -o get-docker.sh
-       sh get-docker.sh
+       	echo "ERROR: Docker not found."
+       	echo "Installing Docker from get.docker.com"
+       	curl -fsSL https://get.docker.com -o get-docker.sh
+    	sh get-docker.sh
 
-	   echo "Installing docker-compose from docs.docker.com/compose/install/"
+	   	echo "Installing docker-compose from docs.docker.com/compose/install/"
 		COMPOSE_VERSION=`git ls-remote https://github.com/docker/compose | grep refs/tags | grep -oE "[0-9]+\.[0-9][0-9]+\.[0-9]+$" | sort --version-sort | tail -n 1`
 		sudo sh -c "curl -L https://github.com/docker/compose/releases/download/${COMPOSE_VERSION}/docker-compose-`uname -s`-`uname -m` > /usr/local/bin/docker-compose"
 		sudo chmod +x /usr/local/bin/docker-compose
